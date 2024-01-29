2659705c4e76fdf1578d8f9860e1e03839c524f7
coderomayer <omayerahmad1998@gmail.com>
2024-01-29 19:04:53 +0600

    Explore page update

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -4,47 +4,52 @@ import React from 'react';
 
 const Explore = () => {
   return (
-    <div className="bg-gray-100 min-h-screen py-12">
+    <div className="bg-gray-100 min-h-screen py-12 md:p-24 max-w-4xl mx-auto font-Quicksand">
       <div className="container mx-auto">
-        <h1 className="text-4xl font-bold mb-8 text-center">Explore OmniTask</h1>
+        <h1 className="text-2xl md:text-5xl font-extrabold mb-8 ">Journey into Efficiency: Unveiling the Wonders of OmniTask</h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          {/* Left Column */}
-          <div className="text-lg">
+        <div className="grid grid-cols-1  gap-6">
+
+          <div>
+            <img className='rounded-md' src="https://img.freepik.com/free-vector/goals-habits-tracking-application-template_23-2148676844.jpg?w=1800&t=st=1706533280~exp=1706533880~hmac=9f32f12d962cc284192311ee225c33cbb96d9b30d4920a1c9e7cb202a556da95" alt="" />
+          </div>
+          
+          <div className="text-sm md:text-lg">
             <p>
-              Welcome to the heart of OmniTask, where collaboration meets
-              productivity. Explore the unique features that make OmniTask the
-              ultimate platform for efficient task management and shared
-              learning experiences.
-            </p>
-            <p className="mt-4">
-              Discover the power of creating tasks, managing deadlines, and
-              seamlessly collaborating with peers. OmniTask is designed to
-              enhance your educational journey.
+            Welcome to the immersive realm of OmniTask, where collaboration seamlessly intertwines with productivity. Join us as we delve into the distinctive features that make OmniTask the epitome of efficient task management and shared learning experiences.
             </p>
           </div>
 
-          {/* Right Column */}
-          <div className="text-lg">
+          
+          <div className="text-sm md:text-lg">
+            <h2 className='font-bold text-blue-600 mb-2'>Discover Dynamic Task Management:</h2>
             <p>
-              OmniTask brings together students from various backgrounds,
-              fostering a community where ideas are shared, tasks are managed,
-              and collaboration thrives. Explore study groups, shared projects,
-              and the joy of achieving academic goals together.
+            Unleash the power of OmniTask by exploring its robust features for creating and managing tasks with utmost efficiency. From setting deadlines to seamlessly collaborating with peers, OmniTask is meticulously crafted to elevate your educational journey. Dive into a world where every task is a step towards academic excellence.
             </p>
-            <p className="mt-4">
-              Dive into a world where learning is interactive, engaging, and
-              tailored to your needs. OmniTask invites you to explore
-              possibilities and redefine your educational experience.
-            </p>
+           
           </div>
+
+          <div className="text-sm md:text-lg">
+            <h2 className='font-bold text-blue-600 mb-2'>A Hub of Collaborative Learning:</h2>
+            <p>
+            OmniTask goes beyond individual achievements, bringing together students from diverse backgrounds to form a thriving community. Here, ideas flow freely, tasks are managed effortlessly, and collaboration becomes the cornerstone of success. Explore the richness of study groups, engage in shared projects, and experience the joy of achieving academic milestones collectively.
+            </p>
+           
+          </div>
+          <div className="text-sm md:text-lg">
+            <h2 className='font-bold text-blue-600 mb-2'>Interactive Learning Redefined:</h2>
+            <p>
+            Immerse yourself in a world where learning transcends traditional boundaries. OmniTask invites you to discover a dynamic landscape where education becomes interactive, engaging, and tailored to your unique needs. It's not just a platform; it's an invitation to explore endless possibilities and redefine your educational journey.
+            </p>
+           
+          </div>
+
         </div>
 
         {/* Call-to-Action */}
         <div className="text-center mt-8">
-          <p className="text-lg mb-4">
-            Ready to start exploring OmniTask? Sign up today and be a part of
-            our collaborative learning community.
+          <p className="text-sm md:text-lg mb-4">
+          As you step into the world of OmniTask, be prepared to unlock a new era of educational excellence where collaboration and productivity converge to shape a brighter future. Explore, engage, and redefine your educational experience with OmniTask.
           </p>
           <button className="bg-blue-500 text-white py-2 px-6 rounded-full">
             Sign Up
